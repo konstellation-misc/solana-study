5a9b9da5e00507177bd378f7e94c1ca80d2bc343
Lalit Shanker Choudhary <lalitshankar@gmail.com>
2022-07-18 14:05:42 +0530

    Zero to production until 3.5

---
+++ zero_to_production/lalit/chapter_3/src/lib.rs
@@ -0,0 +1,30 @@
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer};
+use std::net::TcpListener;
+
+async fn health_check() -> HttpResponse {
+  HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+  let server = HttpServer::new(|| {
+  App::new()
+  .route("/health_check", web::get().to(health_check))
+  })
+  .listen(listener)?
+  .run();
+  Ok(server)
+}
+
+// We need to mark `run` as public.
+// It is no longer a binary entrypoint, therefore we can mark it as async
+// without having to use any proc-macro incantation.
+/*pub fn run(address: &str) -> Result<Server, std::io::Error> {
+  let server = HttpServer::new(|| {
+  App::new()
+  .route("/health_check", web::get().to(health_check))
+  })
+  .bind(address)?
+  .run();
+  Ok(server)
+}*/
---
+++ zero_to_production/lalit/chapter_3/src/main.rs
@@ -0,0 +1,60 @@
+//! main.rs
+use zero2prod::run;
+use std::net::TcpListener;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    run(TcpListener::bind("127.0.0.1:8000").unwrap())?.await
+}
+
+/*
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+async fn health_check(req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check));
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}*/
+
+/*
+use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
+}
+
+#
+async fn health_check(req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+        .route("/health_check", web::get().to(health_check));
+        App::new()
+            .route("/hello", web::get().to(|| async { "Hello World!" }))
+            .service(greet)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}*/
